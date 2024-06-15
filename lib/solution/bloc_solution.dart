d8d8c73b504ad9a6ca820d40bca8feb1a2a01603
주승현 <tmdgus4475@gmail.com>
2024-06-15 13:47:26 +0900

    실습

--- lib/solution/bloc_solution.dart
+++ lib/solution/bloc_solution.dart
@@ -8,72 +8,58 @@ class CounterDecrementEvent extends CounterEvent {}
 
 // Bloc 상태 관리 클래스 정의
 class CounterBloc extends Bloc<CounterEvent, int> {
-  CounterBloc() : super(0);
-
-  @override
-  Stream<int> mapEventToState(CounterEvent event) async* {
-    if (event is CounterIncrementEvent) {
-      yield state + 1;
-    } else if (event is CounterDecrementEvent) {
-      yield state - 1;
-    }
+  CounterBloc() : super(0) {
+    on<CounterIncrementEvent>((event, emit) {
+      emit(state + 1);
+    });
+    on<CounterDecrementEvent>((event, emit) {
+      emit(state - 1);
+    });
   }
 }
 
-void main() {
-  runApp(
-    BlocProvider(
-      create: (context) => CounterBloc(),
-      child: MyApp(),
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
+class BlocScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: BlocExample(),
-    );
-  }
-}
-
-class BlocExample extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Bloc Example')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // CounterBloc의 상태를 구독하고 UI를 업데이트하는 BlocBuilder 위젯
-            BlocBuilder<CounterBloc, int>(
-              builder: (context, count) {
-                return Text(
-                  'Count: $count',
-                  style: TextStyle(fontSize: 48),
-                );
-              },
+    return BlocProvider(
+      create: (_) => CounterBloc(),
+      child: Builder(
+        builder: (context) {
+          return Scaffold(
+            appBar: AppBar(title: Text('Bloc Example')),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  BlocBuilder<CounterBloc, int>(
+                    builder: (context, count) {
+                      return Text(
+                        'Count: $count',
+                        style: TextStyle(fontSize: 48),
+                      );
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FloatingActionButton(
+                        heroTag: 'bloc_increment',
+                        onPressed: () => context.read<CounterBloc>().add(CounterIncrementEvent()),
+                        child: Icon(Icons.add),
+                      ),
+                      SizedBox(width: 20),
+                      FloatingActionButton(
+                        heroTag: 'bloc_decrement',
+                        onPressed: () => context.read<CounterBloc>().add(CounterDecrementEvent()),
+                        child: Icon(Icons.remove),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FloatingActionButton(
-                  // CounterIncrementEvent를 추가하여 카운터 값을 증가
-                  onPressed: () => context.read<CounterBloc>().add(CounterIncrementEvent()),
-                  child: Icon(Icons.add),
-                ),
-                SizedBox(width: 20),
-                FloatingActionButton(
-                  // CounterDecrementEvent를 추가하여 카운터 값을 감소
-                  onPressed: () => context.read<CounterBloc>().add(CounterDecrementEvent()),
-                  child: Icon(Icons.remove),
-                ),
-              ],
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
